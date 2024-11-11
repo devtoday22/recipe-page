d443341fa78110df9f64867460845e0e0d435e9f
Levan Jgerenaia <devtoday22@gmail.com>
2024-11-11 15:54:34 +0400

    added new font and image. also introduce part component

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Noto+Serif+Georgian:wght@100&family=Poppins:wght@100;200&family=Young+Serif&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,27 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {/* Body part of web app */}
+      <main className={styles.mainContainer}>
+        <div className={styles.coverImage}>
+        <Image
+          src="/image-omelette.jpeg"
+          width={600}
+          height={600}
+          alt="Picture of the author"
+        />
+         </div>
+         {/* Introduce part */}
+         <div className={styles.introduce}>
+          <div>
+           <h1>Simple Omelette Recipe</h1> 
+          </div>
+          <div>
+            <p>An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked to perfection, optionally filled with your choice of cheese, vegetables, or meats.</p>
+          </div>
+         </div>
+      </main>
+
      
     </>
   );
